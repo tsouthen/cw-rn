4400885532bf49b51dc950b6b9803089db5a8254
Todd Southen <tsouthen@gmail.com>
2022-08-23 20:01:44 -0700

    Changed non-focused color to black for tab bar icons as they get tinted by the component.

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -4,5 +4,5 @@ import { Icon } from 'react-native-elements';
 
 export default function TabBarIcon(props) {
   const {focused} = props;
-  return (<Icon size={32} color={focused ? Colors.tabIconSelected : Colors.tabIconDefault} {...props} />);
+  return (<Icon size={32} color={focused ? Colors.tabIconSelected : 'black'} {...props} />);
 };
