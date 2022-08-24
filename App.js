13edd2936357965743e41012de3fb2d0d48f7874
Todd Southen <tsouthen@gmail.com>
2022-08-23 20:01:44 -0700

    Added SafeAreaView for iOS

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
 import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Platform, StatusBar, StyleSheet, View, SafeAreaView } from 'react-native';
 import AppNavigator from './navigation/AppNavigator';
 import { Entypo, MaterialIcons, FontAwesome, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import Colors from './constants/Colors';
 
 export default function App(props) {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
@@ -18,10 +19,12 @@ export default function App(props) {
     );
   } else {
     return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
+      <SafeAreaView style={{ flex: 1, backgroundColor: Colors.primaryDark }}>
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="light-content" />}
+          <AppNavigator />
+        </View>
+      </SafeAreaView>
     );
   }
 }
@@ -56,6 +59,6 @@ function handleFinishLoading(setLoadingComplete) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: Colors.primary,
   },
 });
